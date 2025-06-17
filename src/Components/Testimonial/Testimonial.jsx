5e1aa78398f97c50f755cf20fb3be0cf700ae91a
AbhisekSahu009 <abhi2004sahu@gmail.com>
2025-06-17 16:19:34 +0530

    Testimonial completed

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import TestimonialCss from './../Testimonial/Testimonial.module.css'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
+import clientImg1 from './../../assets/hero.jpg'
+import clientImg2 from './../../assets/hero.jpg'
+import clientImg3 from './../../assets/hero.jpg'
+import clientImg4 from './../../assets/hero.jpg'
 import TeamCss from "../Team/Team.module.css";
 
 function Testimonial() {
@@ -26,14 +30,80 @@ function Testimonial() {
 
 
                 <SwiperSlide>
-                    <div className={TestimonialCss.test_card}>
-                        <div className={TestimonialCss.test_info}>
-                            <h2>Nick Davis</h2>
-                            <h5>UI/UX Designer</h5>
+                    <div className={TestimonialCss.test_box}>
+                        <div className={TestimonialCss.test_card}>
+                            <div className={TestimonialCss.test_info}>
+                                <h2>Nick Davis</h2>
+                                <h5>UI/UX Designer</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        <div className={TestimonialCss.client_img}>
+                            <img src={clientImg1} alt=""/>
+                        </div>
                     </div>
                 </SwiperSlide>
+
+
+
+
+
+                <SwiperSlide>
+                    <div className={TestimonialCss.test_box}>
+                        <div className={TestimonialCss.test_card}>
+                            <div className={TestimonialCss.test_info}>
+                                <h2>Nick Davis</h2>
+                                <h5>UI/UX Designer</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+
+                        </div>
+                        <div className={TestimonialCss.client_img}>
+                            <img src={clientImg2} alt=""/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+
+
+
+                <SwiperSlide>
+                    <div className={TestimonialCss.test_box}>
+                        <div className={TestimonialCss.test_card}>
+                            <div className={TestimonialCss.test_info}>
+                                <h2>Nick Davis</h2>
+                                <h5>UI/UX Designer</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+
+                        </div>
+                        <div className={TestimonialCss.client_img}>
+                            <img src={clientImg3} alt=""/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+
+
+
+
+                <SwiperSlide>
+                    <div className={TestimonialCss.test_box}>
+                        <div className={TestimonialCss.test_card}>
+                            <div className={TestimonialCss.test_info}>
+                                <h2>Nick Davis</h2>
+                                <h5>UI/UX Designer</h5>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+
+                        </div>
+                        <div className={TestimonialCss.client_img}>
+                            <img src={clientImg4} alt=""/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
 
 
 
