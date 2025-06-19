fc8747c2615d15f2a9e9003abe826914ec5b4bab
AbhisekSahu009 <abhi2004sahu@gmail.com>
2025-06-19 21:13:53 +0530

    Minor changes in copyright and footer done

--- src/Components/Copyright/Copyright.jsx
+++ src/Components/Copyright/Copyright.jsx
@@ -4,7 +4,7 @@ import CopyrightCss from './../Copyright/Copyright.module.css'
 function Copyright() {
     return (
         <section className={CopyrightCss.copyright}>
-            <p>@Copyright 2025.Made with <a href="#">❤</a> by Code X Novas </p>
+            <p>@Copyright 2025.Made with <a href="#">❤</a> by Code-X-Novas </p>
         </section>
     )
 }
